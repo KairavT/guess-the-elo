c31c4eabcbb67a7cfe050bf5ee7d09d1511f8290
Kairav <kairavtupil@gmail.com>
2026-08-18 20:19:20 -0400

    added config constants

--- scripts/config.py
+++ scripts/config.py
@@ -1,1 +1,6 @@
-# shared constants: dump URL, filter thresholds, data paths, hyperparameters
+URL = 'https://database.lichess.org/standard/lichess_db_standard_rated_2026-06.pgn.zst'
+NUM_GAMES = 100000
+ALLOWED_CATEGORIES = ['Rated Blitz game', 'Rated Rapid game']
+MIN_MOVES = 10
+OUTPUT_PATH = 'data/games.jsonl'
+RANDOM_SEED = 123

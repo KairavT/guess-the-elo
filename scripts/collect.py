bdff0b83d0216aa499c1a64d8f8d84b1708c8191
Kairav <kairavtupil@gmail.com>
2026-08-22 17:09:57 -0400

    implemented counter to see which causes the most games to be rejected:

--- scripts/collect.py
+++ scripts/collect.py
@@ -9,19 +9,26 @@ with requests.get(URL, stream=True) as response:
 
     count_games = 0
     limit = 100
+    reject_counter = {"Wrong Event": 0,
+                      "Bot Game": 0,
+                      "Too Short":0}
     while True:
         game = chess.pgn.read_game(reader_text)
         if not game or count_games == limit:
             break
         if game.headers.get('Event')\
               not in ALLOWED_CATEGORIES:
+            reject_counter['Wrong Event'] += 1
             continue
         if game.headers.get('WhiteTitle') == 'BOT' or \
             game.headers.get('BlackTitle') == 'BOT':
+            reject_counter['Bot Game'] += 1
             continue
         if game.end().ply() < MIN_PLY:
+            reject_counter['Too Short'] += 1
             continue
 
         count_games += 1
     print(count_games)
+    print(reject_counter)
 

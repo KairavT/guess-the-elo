d1f8ce6f0b0a5341f01003a27a2e5db16789bba5
Kairav <kairavtupil@gmail.com>
2026-08-25 23:50:51 -0400

    update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -8,3 +8,5 @@ SCRIPT_DIR = Path(__file__).resolve().parent
 OUTPUT_PATH = SCRIPT_DIR.parent / 'data' / 'games.jsonl'
 RANDOM_SEED = 123
 TEST_FRACTION = 0.2 
+TRAIN_PATH = SCRIPT_DIR.parent / 'data' / 'games_train.jsonl'
+TEST_PATH = SCRIPT_DIR.parent / 'data' / 'games_test.jsonl'
